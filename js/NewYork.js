bdbb458037c0a283af8ae9e1d173d12de9315887
amandelman <almondman@gmail.com>
2016-04-21 10:15:42 -0500

    Leaflet basemap

--- js/NewYork.js
+++ js/NewYork.js
@@ -2,16 +2,17 @@
 
 function createMap(){
 //initialize the map on the "map" div with a given center aand zoom level
-    var map = L.map("map").setView([32.5, -80], 4);
+ var map = L.map("map").setView([40.8, -74], 12);
 
 //load and display a tile layer on the map
-    var Stamen_Toner = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
-	       attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> and <a href="http://www.cfr.org/interactives/GH_Vaccine_Map/#introduction">Council on Foreign Relations</a>, "Vaccine-Preventable Outbreaks," 2015. Sequencer buttons courtesy of Clockwise.',
-	       subdomains: 'abcd',
-	       minZoom: 4,
-	       maxZoom: 5,
-	       ext: 'png'
-    }).addTo(map);
+    var CartoDB_Positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map);
 
     getData(map);
 };
+
+createMap();
+
